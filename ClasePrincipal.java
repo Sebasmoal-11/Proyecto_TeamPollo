f800e7701386eefe8e4011553c4faed17d28882d
CaledSV <vasquez.solis.steven@gmail.com>
2024-11-23 22:11:32 -0600

    origin/main

--- ClasePrincipal.java
+++ ClasePrincipal.java
@@ -1,7 +1,7 @@
 public class ClasePrincipal {
 
     public static void main(String[] args) {
-        System.out.println("prueba numero 1");
-        System.out.println("Prueba #2");
+        System.out.println("prueba numero 1"); //sample comment gjhyjgh
+        System.out.println("Prueba #2"); //Comment de prueba numero 3
     }
 }
